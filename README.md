0723876a6a8142e754669d1a6282e06c63734aa5
lkqm <luokaiqiongmou@foxmail.com>
2021-10-11 21:12:16 +0800

    fix: 修复兼容性问题

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Lint git commit message, author, branch
 - user.name: must be account
 - user.email: must be email
 - message: Angular JS commit message
+
+rules: [rules.json](src/main/resources/rules.json)
--- src/main/java/io/commitlint/InternalUtils.java
+++ src/main/java/io/commitlint/InternalUtils.java
@@ -2,11 +2,12 @@ package io.commitlint;
 
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import org.apache.commons.io.IOUtils;
 
 public class InternalUtils {
 
@@ -14,12 +15,28 @@ public class InternalUtils {
     }
 
     public static List<LintRule> getLintRules() {
-        try {
-            InputStream is = InternalUtils.class.getClassLoader().getResourceAsStream(DefaultConstants.RULES_FILE);
-            String json = IOUtils.toString(is, StandardCharsets.UTF_8.name());
-            return new Gson().fromJson(json, new TypeToken<List<LintRule>>(){}.getType());
+        try (InputStream is = InternalUtils.class.getClassLoader().getResourceAsStream(DefaultConstants.RULES_FILE);) {
+            String json = read(is);
+            return new Gson().fromJson(json, new TypeToken<List<LintRule>>() {
+            }.getType());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String read(InputStream is) {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));) {
+            while (true) {
+                String line = bufferedReader.readLine();
+                if (line == null) {
+                    break;
+                }
+                sb.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sb.toString();
     }
 }
